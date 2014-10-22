deead709982c236f9e85cfa7b2074521cd60ab0b
Jon Atkins <Jon@Jons-MacBook-Pro-2.local>
2014-10-22 14:41:34 +0200

    Initial Commit

---
+++ Graphics Curve Editor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graphics Curve Editor
+//
+//  Created by Jon Atkins on 10/22/14.
+//  Copyright (c) 2014 AIT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
